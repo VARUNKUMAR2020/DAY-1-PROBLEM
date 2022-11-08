63a738eeb37e5c0c68ae7c4cb82db971a26bb424
VARUNKUMAR2020 <varunvenkat2020@gmail.com>
2022-11-08 23:11:12 +0530

    Add:Sum Command Line Arguments And Cound Invalid Formats

--- Practice.java
+++ Practice.java
@@ -42,5 +42,18 @@ public class Practice {
             System.err.println("Given String Are Not Equal");
         }
 
+        // To Sum The Command Line Arguments And Count The Invalid Integers Entered
+
+        int sum = 0;
+        int count = 0;
+        for (int i = 0; i < args.length; i++) {
+            try {
+                sum += Integer.parseInt(args[i]);
+            } catch (NumberFormatException e) {
+                count++;
+            }
+        }
+        System.out.println("Sum = " + sum);
+        System.out.println("Number of invalid integers = " + count);
     }
 }
